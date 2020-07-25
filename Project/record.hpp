c9f1e00b163ae5208a6693712e8da8fb34a0a4ac
waking-eni <61460471+waking-eni@users.noreply.github.com>
2020-07-25 17:51:25 +0200

    Update record.hpp
    fixed weird git spaces

--- Project/record.hpp
+++ Project/record.hpp
@@ -11,17 +11,17 @@
 
 class Record {
     public:
-	    int num;
+	int num;
         std::string name;
-		Record *next;
-		Record() { num = 0; name = ""; }
-		Record* get_node();
-		Record* get_record();
-		Record* searh_node(int id, int *flag);
-		int insert_record(Record *node1);
-		void show_records();
-		int delete_record(std::string name);
-		int edit_record(std::string *record, std::string name, std::string num);
+	Record *next;
+	Record() { num = 0; name = ""; }
+	Record* get_node();
+	Record* get_record();
+	Record* searh_node(int id, int *flag);
+	int insert_record(Record *node1);
+	void show_records();
+	int delete_record(std::string name);
+	int edit_record(std::string *record, std::string name, std::string num);
 };
 
 extern Record *my_list;
@@ -33,16 +33,14 @@ Record* Record::get_node() {
 	std::string num;
 
 	//std::cin.get();
-    std::ifstream my_file ("phonebook.txt");
-    std::getline(my_file, name);
-    std::getline(my_file, num);
-    temp->name = name;
-    temp->num = std::stoi(num);
-    //std::cout << temp->num << " " << temp->name;
-    //std::cin.get();
+    	std::ifstream my_file ("phonebook.txt");
+	    std::getline(my_file, name);
+    	std::getline(my_file, num);
+    	temp->name = name;
+    	temp->num = std::stoi(num);
 
 	temp->next = NULL;
-    return temp;
+    	return temp;
 }
 
 Record* Record::get_record() {
@@ -50,12 +48,12 @@ Record* Record::get_record() {
 	temp = (Record*)malloc(sizeof(Record));
 
 	std::cout << "Enter the phone number: ";
-    std::cin >> temp->num;
-    std::cout << "Enter the name: ";
-    std::cin >> temp->name;
+    	std::cin >> temp->num;
+    	std::cout << "Enter the name: ";
+    	std::cin >> temp->name;
 
-    temp->next = NULL;
-    return temp;
+    	temp->next = NULL;
+    	return temp;
 }
 
 Record* Record::searh_node(int id, int *flag) {
@@ -66,11 +64,11 @@ Record* Record::searh_node(int id, int *flag) {
 	if(my_list == NULL)
 		return NULL;
 	for(prev = NULL, curr = my_list; (curr); prev = curr, curr = curr->next) {
-        if(curr->num == id) {
-            *flag = 1;
-            break;
-        }
-    }
+		if(curr->num == id) {
+		    *flag = 1;
+		    break;
+		}
+    	}
     return prev;
 }
 
@@ -81,20 +79,20 @@ int Record::insert_record(Record *node1) {
 	prev = Record::searh_node(node1->num, &flag);
 
 	if(my_list == NULL) {
-        my_list = node1; //first node
-        return -1;
-    } else {
-        node1->next = prev->next;
-        prev->next = node1; //insert in the middle or end
-        std::ofstream my_file("phonebook.txt", std::ofstream::app);
-        if (my_file.is_open()) {
-            my_file << "\n";
-            my_file << node1->name;
-            my_file << "\n";
-            my_file << std::to_string(node1->num);
-        }
-        my_file.close();
-    }
+        	my_list = node1; //first node
+        	return -1;
+    	} else {
+		node1->next = prev->next;
+		prev->next = node1; //insert in the middle or end
+		std::ofstream my_file("phonebook.txt", std::ofstream::app);
+		if (my_file.is_open()) {
+		    my_file << "\n";
+		    my_file << node1->name;
+		    my_file << "\n";
+		    my_file << std::to_string(node1->num);
+		}
+		my_file.close();
+    	}
 
     return 0;
 }
@@ -103,25 +101,25 @@ void Record::show_records() {
 	Record *curr;
 
 	if(my_list == NULL) {
-        std::cout << "The list is empty!";
-        return;
-    }
+        	std::cout << "The list is empty!";
+        	return;
+    	}
 
-    std::cout << "The records: " << std::endl;
-    std::ifstream my_file ("phonebook.txt");
-    std::string line;
-    if(my_file.is_open()) {
-        while(getline(my_file, line)) {
-            std::cout << line << '\n';
-        }
-    } else {
-        std::cout << "Can't open file.";
-    }
-    my_file.close();
+	std::cout << "The records: " << std::endl;
+	std::ifstream my_file ("phonebook.txt");
+	std::string line;
+	if(my_file.is_open()) {
+		while(getline(my_file, line)) {
+		    std::cout << line << '\n';
+		}
+	} else {
+		std::cout << "Can't open file.";
+	}
+	my_file.close();
 }
 
 int Record::delete_record(std::string name) {
-    std::vector<std::string> buffer;
+    	std::vector<std::string> buffer;
 
 	std::ifstream my_file ("phonebook.txt");
 	std::string line;
@@ -129,25 +127,25 @@ int Record::delete_record(std::string name) {
 	std::ofstream temp("temp.txt");
 
 	if(my_list == NULL)
-        return -1;
+        	return -1;
 
 	if(temp.is_open()) {
-        while(getline(my_file, line)) {
-            if(name == line) {
-                buffer.push_back("aha");
-            }
-            buffer.push_back(line);
-        }
-        std::ostream_iterator<std::string> output_iterator(temp, "\n");
-        std::copy(buffer.begin(), buffer.end() - 1, output_iterator);
-    }
-    my_file.close();
-    temp.close();
+		while(getline(my_file, line)) {
+		    if(name == line) {
+			buffer.push_back("aha");
+		    }
+            	buffer.push_back(line);
+        	}
+        	std::ostream_iterator<std::string> output_iterator(temp, "\n");
+        	std::copy(buffer.begin(), buffer.end() - 1, output_iterator);
+    	}
+    	my_file.close();
+    	temp.close();
 
-    remove("phonebook.txt");
-    rename("temp.txt", "phonebook.txt");
+    	remove("phonebook.txt");
+    	rename("temp.txt", "phonebook.txt");
 
-    return 0;
+    	return 0;
 }
 
 int Record::edit_record(std::string *record, std::string name, std::string num) {
@@ -160,31 +158,30 @@ int Record::edit_record(std::string *record, std::string name, std::string num) 
 	std::ofstream temp("temp.txt");
 
 	if(my_list == NULL)
-        return -1;
+        	return -1;
 
 	if(temp.is_open()) {
-        while(getline(my_file, line)) {
-            if(name == line) {
-                buffer.push_back("aha");
-            }
-            buffer.push_back(line);
-        }
-        std::ostream_iterator<std::string> output_iterator(temp, "\n");
-        std::copy(buffer.begin(), buffer.end(), output_iterator);
-    }
-    my_file.close();
-    temp.close();
+		while(getline(my_file, line)) {
+		    if(name == line) {
+			buffer.push_back("aha");
+		    }
+		    buffer.push_back(line);
+		}
+		std::ostream_iterator<std::string> output_iterator(temp, "\n");
+		std::copy(buffer.begin(), buffer.end(), output_iterator);
+    	}
+	my_file.close();
+	temp.close();
 
-    remove("phonebook.txt");
-    rename("temp.txt", "phonebook.txt");
+	 remove("phonebook.txt");
+	 rename("temp.txt", "phonebook.txt");
 
-    if(rec_exists == 0) {
-        std::cout << "The record doesn't exist!\n";
-        return NULL;
-    }
+	 if(rec_exists == 0) {
+		std::cout << "The record doesn't exist!\n";
+		return NULL;
+	 }
 
-    return 0;
+	 return 0;
 }
 
 #endif // RECORD_HPP_INCLUDED
-
